ccf1b3d833524309eccdeb2f152c8a969e17f5e5
Mrutyu <rout.mrutyunjay@gmail.com>
2024-04-12 10:55:05 -0400

    first commit

---
+++ src/main/java/com/mrutyu/basicDemoAlibu/BasicDemoAlibuApplication.java
@@ -0,0 +1,13 @@
+package com.mrutyu.basicDemoAlibu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicDemoAlibuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicDemoAlibuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrutyu/basicDemoAlibu/BasicDemoAlibuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrutyu.basicDemoAlibu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicDemoAlibuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
